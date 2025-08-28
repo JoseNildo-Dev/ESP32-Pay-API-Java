d48537ce8885daa19244c25297507ea6ce1ce101
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2025-08-28 00:02:47 -0300

    Start: Criar o projeto base
    Projeto SpringBoot base contendo as dependencias:
    Spring Web, Spring Security, Spring Data JPA, Devtools, H2 Database, Validation, Lombok e a API do Mercado pago

---
+++ src/main/java/br/com/jnmpdev/esppayapi/EsppayapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jnmpdev.esppayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsppayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsppayapiApplication.class, args);
+	}
+
+}
